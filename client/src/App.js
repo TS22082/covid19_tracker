a703c8068183edcd92b1406de9ede60758726ca7
TS22082 <ts22082@gmail.com>
2020-03-13 16:20:25 -0700

    seperate map into its own container

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,13 @@
-import axios from "axios";
-import GoogleMapReact from "google-map-react";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import AffectedArea from "./components/AffectedArea/AffectedArea";
-import secret from "./secret";
+import Map from "./components/Map/Map";
 
 import "./App.css";
 
 function App() {
-  const [location, setlocation] = useState({});
-  const [affectedAreas, setAffectedAreas] = useState({ data: [] });
-
-  useEffect(() => {
-    axios.get("/api/confirmed").then(res => {
-      setAffectedAreas({ data: res.data });
-      navigator.geolocation.getCurrentPosition(function(position) {
-        setlocation({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        });
-      });
-    });
-  }, []);
-
   return (
     <div className="App">
-      <div style={{ height: "100vh", width: "100%" }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: secret.GOOGLE_KEY }}
-          defaultCenter={{ lat: 26.8206, lng: 17.2283 }}
-          center={location}
-          defaultZoom={1}
-        >
-          {affectedAreas.data.map((element, index) => (
-            <AffectedArea
-              key={index}
-              lat={element.coordinates.lat}
-              lng={element.coordinates.long}
-              data={{ country: element.country, history: element.history }}
-            />
-          ))}
-        </GoogleMapReact>
-      </div>
+      <Map />
     </div>
   );
 }
---
+++ client/src/components/Map/Map.js
@@ -0,0 +1,45 @@
+import secret from "../../secret";
+import axios from "axios";
+import GoogleMapReact from "google-map-react";
+import React, { useEffect, useState } from "react";
+
+import AffectedArea from "../AffectedArea/AffectedArea";
+
+function Map() {
+  const [location, setlocation] = useState({});
+  const [affectedAreas, setAffectedAreas] = useState({ data: [] });
+
+  useEffect(() => {
+    axios.get("/api/confirmed").then(res => {
+      setAffectedAreas({ data: res.data });
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setlocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        });
+      });
+    });
+  }, []);
+
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: secret.GOOGLE_KEY }}
+        defaultCenter={{ lat: 26.8206, lng: 17.2283 }}
+        center={location}
+        defaultZoom={1}
+      >
+        {affectedAreas.data.map((element, index) => (
+          <AffectedArea
+            key={index}
+            lat={element.coordinates.lat}
+            lng={element.coordinates.long}
+            data={{ country: element.country, history: element.history }}
+          />
+        ))}
+      </GoogleMapReact>
+    </div>
+  );
+}
+
+export default Map;
