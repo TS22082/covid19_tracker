40c2315719f4a940679ea91307cb84ddff3d6c2b
TS22082 <ts22082@gmail.com>
2020-03-13 14:08:01 -0700

    make map center on user location if lcoation services are enabled

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import React, { useEffect } from "react";
-import "./App.css";
+import axios from "axios";
 import GoogleMapReact from "google-map-react";
+import React, { useEffect, useState } from "react";
+
 import AffectedArea from "./components/AffectedArea/AffectedArea";
 import secret from "./secret";
-import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const [location, setlocation] = useState({ lat: 0, lng: 0 });
+
   useEffect(() => {
-    axios.get("/api/confirmed").then(res => console.log(res));
+    axios.get("/api/confirmed").then(res => {
+      console.log(res);
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setlocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        });
+      });
+    });
   }, []);
   return (
     <div className="App">
@@ -15,6 +27,7 @@ function App() {
         <GoogleMapReact
           bootstrapURLKeys={{ key: secret.GOOGLE_KEY }}
           defaultCenter={{ lat: 26.8206, lng: 17.2283 }}
+          center={location}
           defaultZoom={1}
         >
           <AffectedArea lat={59.955413} lng={30.337844} />
