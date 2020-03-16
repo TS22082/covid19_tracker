c099c3ba672c98e33a7feae176ca29bcf8b9920e
TS22082 <ts22082@gmail.com>
2020-03-16 11:38:49 -0700

    render covid-19 articles to screen

--- client/src/components/News/News.js
+++ client/src/components/News/News.js
@@ -1,14 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function News() {
+  const [articles, setArticles] = useState([]);
   useEffect(() => {
-    axios.get("api/news").then(res => console.log(res.data.articles));
+    axios.get("api/news").then(res => {
+      console.log(res.data.articles);
+      setArticles(res.data.articles);
+    });
   }, []);
 
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-sm-12">
+          {articles.map(article => (
+            <div className="card mt-2">
+              <div className="card-body">
+                <h5 className="card-title">{article.title}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  {article.source.name}
+                </h6>
+                <p className="card-text">{article.content}</p>
+                <a href={article.url} target="_blank" className="card-link">
+                  Article link
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
