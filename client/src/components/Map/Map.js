f08a596a24fec59507047c8e57225a85a481f24f
TS22082 <ts22082@gmail.com>
2020-03-18 13:57:15 -0700

    add loading screen while news and maps load

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import GoogleMapReact from "google-map-react";
 import React, { useEffect, useState } from "react";
-
 import AffectedArea from "../AffectedArea/AffectedArea";
+import Loader from "../Loader/Loader";
 
 function Map() {
   const [location, setlocation] = useState({});
   const [affectedAreas, setAffectedAreas] = useState({ data: [] });
+  const [loading, setLoading] = useState({ loaded: false });
+
   useEffect(() => {
     axios.get("/api/confirmed").then(res => {
       setAffectedAreas({ data: res.data });
@@ -16,47 +18,58 @@ function Map() {
           lng: position.coords.longitude
         });
       });
+      setLoading({ loaded: true });
     });
   }, []);
 
-    const handleApiLoaded = (map, maps) => {
-        affectedAreas.data.forEach(element => {
-            if (element.country_code === 'US') {
-                if (element.county && element.state) {
-                    map.data.loadGeoJson(`/api/geojson?county=${element.county}`);
-                }
-            }
-        });
-        map.data.setStyle(function(feature) {
-            return /** @type {!google.maps.Data.StyleOptions} */({
-                fillColor: 'red',
-                strokeColor: 'red',
-                strokeWeight: 2
-            });
-        });
-    };
+  const handleApiLoaded = (map, maps) => {
+    affectedAreas.data.forEach(element => {
+      if (element.country_code === "US") {
+        if (element.county && element.state) {
+          map.data.loadGeoJson(`/api/geojson?county=${element.county}`);
+        }
+      }
+    });
+    map.data.setStyle(function(feature) {
+      return /** @type {!google.maps.Data.StyleOptions} */ ({
+        fillColor: "red",
+        strokeColor: "red",
+        strokeWeight: 2
+      });
+    });
+  };
 
   return (
-    <div style={{ height: "100vh", width: "100%" }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_KEY }}
-        defaultCenter={{ lat: 26.8206, lng: 17.2283 }}
-        center={location}
-        defaultZoom={1}
-        yesIWantToUseGoogleMapApiInternals
-        onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
-      >
-        {
-            affectedAreas.data.map((element, index) => {
-                return <AffectedArea
-                    key={index}
-                    lat={element.coordinates.lat}
-                    lng={element.coordinates.long}
-                    data={{ country: element.country, history: element.history, latest: element.latest }}
+    <div>
+      {!loading.loaded ? (
+        <Loader />
+      ) : (
+        <div style={{ height: "100vh", width: "100%" }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_KEY }}
+            defaultCenter={{ lat: 26.8206, lng: 17.2283 }}
+            center={location}
+            defaultZoom={1}
+            yesIWantToUseGoogleMapApiInternals
+            onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+          >
+            {affectedAreas.data.map((element, index) => {
+              return (
+                <AffectedArea
+                  key={index}
+                  lat={element.coordinates.lat}
+                  lng={element.coordinates.long}
+                  data={{
+                    country: element.country,
+                    history: element.history,
+                    latest: element.latest
+                  }}
                 />
-        }
-        )}
-      </GoogleMapReact>
+              );
+            })}
+          </GoogleMapReact>
+        </div>
+      )}
     </div>
   );
 }
--- client/src/components/News/News.js
+++ client/src/components/News/News.js
@@ -1,40 +1,49 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Loader from "../Loader/Loader";
 
 function News() {
   const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState({ loaded: false });
+
   useEffect(() => {
     axios.get("api/news").then(res => {
-      console.log(res.data.articles);
       setArticles(res.data.articles);
+      setLoading({ loaded: true });
     });
   }, []);
 
   return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col-sm-12">
-          {articles.map(article => (
-            <div className="card mt-2">
-              <div className="card-body">
-                <h5 className="card-title">{article.title}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">
-                  {article.source.name}
-                </h6>
-                <p className="card-text">{article.content}</p>
-                <a
-                  href={article.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="card-link"
-                >
-                  Article link
-                </a>
-              </div>
+    <div>
+      {!loading.loaded ? (
+        <Loader />
+      ) : (
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-sm-12">
+              {articles.map((article, index) => (
+                <div className="card mt-2" key={index}>
+                  <div className="card-body">
+                    <h5 className="card-title">{article.title}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">
+                      {article.source.name}
+                    </h6>
+                    <p className="card-text">{article.content}</p>
+                    <a
+                      href={article.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="card-link"
+                    >
+                      Article link
+                    </a>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
